4720cfadc6ebb5e4d6740f1065525a8280f67619
NewSnooker <nu0075533@gmail.com>
2025-04-06 13:16:33 +0700

    Implement user update functionality and improve error handling in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -23,21 +23,66 @@ export const UserController = {
           status: "active",
         },
       });
+
       if (!user) {
-        return {
-          message: "Invalid username or password",
-        };
+        return { message: "Invalid username or password" };
       }
+
+      // ใช้ jwt.sign() ที่ส่งมาจาก route
       const token = await jwt.sign(user);
+
       return {
         user,
         token,
       };
     } catch (error) {
       console.error(error);
-      return {
-        message: "Internal server error",
+      return { message: "Internal server error" };
+    }
+  },
+  update: async ({
+    body,
+    set,
+    request,
+    jwt,
+  }: {
+    body: { username: string; password: string };
+    set: any;
+    request: any;
+    jwt: any;
+  }) => {
+    try {
+      const headers = request.headers.get("Authorization");
+      const token = headers.split(" ")[1];
+      const payload = await jwt.verify(token);
+      const id = payload.id;
+      const newData = {
+        username: body.username,
+        password: body.password,
       };
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!user) {
+        set.status = 404;
+        return { message: "User not found" };
+      }
+
+      await prisma.user.update({
+        where: {
+          id: id,
+        },
+        data: newData,
+      });
+      set.status = 200;
+      return { message: "User updated successfully" };
+    } catch (error) {
+      console.error(error);
+      return { message: "Internal server error" };
     }
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { Elysia } from "elysia";
-import { jwt } from "@elysiajs/jwt";
 import { cors } from "@elysiajs/cors";
 import { UserController } from "./controllers/UserController";
+import jwt from "@elysiajs/jwt";
 
 const app = new Elysia()
+  .use(cors({ origin: "http://localhost:3000", credentials: true }))
+  .use(
+    jwt({
+      name: "jwt",
+      secret: "your_secret_key",
+    })
+  )
   .get("/", () => "Hello Elysia")
   .post("/api/user/signin", UserController.signIn)
+  .put("/api/user/update", UserController.update)
   .listen(5000);
 
 console.log(
